523c2dd03f8f0ef25aac21827be82d29aac78600
Pedro <pedro.roseno@invillia.com>
2023-04-03 00:21:31 -0300

    Project created

---
+++ src/main/java/com/pedrohroseno/vehiclessalesmanager/VehiclesSalesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohroseno.vehiclessalesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesSalesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclesSalesManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrohroseno/vehiclessalesmanager/VehiclesSalesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohroseno.vehiclessalesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesSalesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
